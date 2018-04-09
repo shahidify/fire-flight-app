8796aedd5eb2cd5eaafac0441cfc052b28315603
Shahid Khan <shahid.manit@gmail.com>
2018-04-09 17:15:12 -0500

    Handle submit and change event
    Handle submit and change event

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -18,17 +21,29 @@ class App extends Component {
       })
     } ) ;
   }
-
+  handleSubmit(event) {
+    event.preventDefault();
+    database.ref().child('superNewData').set(this.state.newData);
+  }
+  handleChange(event) {
+    const newData = event.target.value;
+    this.setState({
+      newData: newData
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App--header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <pre className="App-intro">
+        <pre className="App-data">
           { JSON.stringify(this.state.data, null, 2) }
         </pre>
-        
+        <form className="App--form" onSubmit={this.handleSubmit} >
+          <input type="text" value={this.state.newData} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
